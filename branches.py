1817b9983dd398c751499b6702723b024bf5b150
Leonardo Castano <leonardo.castano@gmail.com>
2023-06-16 16:32:13 +0000

    DEV-001 Branches

--- branches.py
+++ branches.py
@@ -11,8 +11,6 @@ projects_db=['project1key','project2key','project3key']
 sonar = SonarQubeClient(sonarqube_url=url, username=user, password=passwd)
 def get_main_branch_name(project_key):
     branches = sonar.project_branches.search_project_branches(project=project_key)
-    #for key, value in branches.items():
-      #print(f'{key},{value}')
     for branch in branches["branches"]:
         if branch['isMain']:
             print(f"Project Name: {project_key}, is Main: {branch['isMain']}, Branch Name: {branch['name']}")
@@ -20,13 +18,6 @@ def get_main_branch_name(project_key):
 
 def get_projects_names():
     components = sonar.projects.search_projects()
-   # for component in components:
-    #    print(f"{components.get('key')}")
     for component in components["components"]:
         get_main_branch_name(component["key"])
-            #print(f"Component: {component['key']}")
-        #projectkey = project['key']
-    #get_main_branch_name(projectkey)
-# components [{'key': 'project1key', 'name': 'project1', 'qualifier': 'TRK', 'visibility': 'public'}, {'key': 'project2key', 'name': 'project2', 'qualifier': 'TRK', 'visibility': 'public'}, {'key': 'project3key', 'name': 'project3', 'qualifier': 'TRK', 'visibility': 'public'}]
 get_projects_names()    
-#get_main_branch_name('')
