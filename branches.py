3a9ed04caa1254be1c848fb5c38b0faa2588c9c3
Leonardo Castano <leonardo.castano@gmail.com>
2023-06-17 23:35:52 +0000

    DEV-001 Write a CSV File out of a SonarQube query

--- branches.py
+++ branches.py
@@ -3,21 +3,37 @@ import os, json, csv
 from sonarqube import SonarQubeClient
 from dotenv import load_dotenv
 load_dotenv()
-token=os.environ.get('token')
-user=os.environ.get('user')
-passwd=os.environ.get('passwd')
-url=os.environ.get('url')
-projects_db=['project1key','project2key','project3key']
+token = os.environ.get('token')
+user = os.environ.get('user')
+passwd = os.environ.get('passwd')
+url = os.environ.get('url')
 sonar = SonarQubeClient(sonarqube_url=url, username=user, password=passwd)
-def get_main_branch_name(project_key):
-    branches = sonar.project_branches.search_project_branches(project=project_key)
-    for branch in branches["branches"]:
-        if branch['isMain']:
-            print(f"Project Name: {project_key}, is Main: {branch['isMain']}, Branch Name: {branch['name']}")
-      
+
+
+
+def store_file_csv(branches_list):
+    headerList = ['Project', 'Branch']
+    with open('branches.csv', 'w') as file:
+        writer = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        for branch in branches_list:
+            writer.writerows([[branch]])
+
+
+def get_main_branch_name(projects_keys):
+    branch_to_print = []
+    branch_to_print.append("PROJECT_NAME, BRANCH_NAME")
+    for project_key in projects_keys:
+        branches = sonar.project_branches.search_project_branches(project=project_key)
+        for branch in branches["branches"]:
+            if branch['isMain']:
+                branch_to_print.append(project_key+", "+branch['name'])
+    store_file_csv(branch_to_print)
 
 def get_projects_names():
     components = sonar.projects.search_projects()
+    projects_keys = []
     for component in components["components"]:
-        get_main_branch_name(component["key"])
+        projects_keys.append(component["key"])
+    get_main_branch_name(projects_keys)
+
 get_projects_names()    
